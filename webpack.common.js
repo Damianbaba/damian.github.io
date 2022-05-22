e8e6d20b5b3d7781e839fd2c1e9b3ef10349058c
Damian Mroczek <damianmroczek@yahoo.com>
2022-05-22 16:43:41 +0200

    index.html section hero

--- webpack.common.js
+++ webpack.common.js
@@ -82,11 +82,6 @@ module.exports = {
       chunks: ["index"],
       filename: "index.html",
     }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/another.html",
-      inject: true,
-      chunks: ["index", "another"],
-      filename: "another.html",
-    }),
+
   ],
 };
