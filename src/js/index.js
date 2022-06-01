f4bb07d5c957db2a491dfc16f19c29861049b3f5
Damian Mroczek <damianmroczek@yahoo.com>
2022-06-01 16:32:12 +0200

    feat: js hand update

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,12 @@ import '../scss/main.scss';
 
 console.log("Hi, I'm Damian - nice to meet you");
 
-
 fetch('https://api.github.com/users/Damianbaba/repos')
     .then((res) => res.json())
     .then((res) => {
         const container = document.querySelector('.projects-grid--js');
         for (let repo of res) {
-
             const { description, homepage, html_url, name } = repo;
-
             const template = `<article class="project">
           <div class="project__window">
             <span class="project__circle"></span>
@@ -33,7 +30,6 @@ fetch('https://api.github.com/users/Damianbaba/repos')
             if (description) {
                 container.innerHTML += template;
             }
-
         }
     })
     .catch((e) => console.log(e));
